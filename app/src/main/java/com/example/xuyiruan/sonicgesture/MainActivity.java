3936e6c4f994b719d8b889527342784a3a1d2c29
Yunhe L <liu348@wisc.edu>
2015-10-07 21:40:02 -0500

    Added start/pause functionality

--- app/src/main/java/com/example/xuyiruan/sonicgesture/MainActivity.java
+++ app/src/main/java/com/example/xuyiruan/sonicgesture/MainActivity.java
@@ -7,20 +7,44 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.media.AudioManager;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //initiate start/stop button
+    private Button startBtn;
+    //if startCount is odd, start to play music, if even, pause play music
+    int startCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.audio);
-        //media player for playing music/soundclip
-        AudioManager audioManager= (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-        //system service to manage output device,
-        mediaPlayer.start();
+        //set up start/pause button
+        startBtn = (Button) findViewById(R.id.startBtn);
 
+        startBtn.setOnClickListener(new View.OnClickListener() {
+
+            //set up the mediaPlayer and link the music to raw (canon in d)
+            MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.audio);
+
+            //media player for playing music/soundclip
+            AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+            public void onClick(View view) {
+                //update startCount for stop/pause button functionality
+                startCount++;
+
+                //system service to manage output device,
+                if(startCount%2 == 0){
+                    mediaPlayer.pause();
+                }else{
+                    mediaPlayer.start();
+                }
+            }
+        });
     }
 
     @Override
