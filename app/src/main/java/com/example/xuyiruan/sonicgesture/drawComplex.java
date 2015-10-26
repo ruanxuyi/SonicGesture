7715a9c2ca459fa950eb00398b9765b61054f484
kofe <fkf2fkf2fkf2@126.com>
2015-10-25 21:23:43 -0500

    kefei's version

---
+++ app/src/main/java/com/example/xuyiruan/sonicgesture/drawComplex.java
@@ -0,0 +1,102 @@
+package com.example.xuyiruan.sonicgesture;
+
+/**
+ * Created by kofe on 10/25/15.
+ */
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.view.View;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class drawComplex extends View{
+    private int XPoint = 60;
+    private int YPoint = 260;
+    private int XScale = 8;
+    private int YScale = 40;
+    private int XLength = 380;
+    private int YLength = 240;
+
+    private int MaxDataSize = XLength / XScale;
+
+    private List<Integer> data = MainActivity.datas;
+
+
+
+    private String[] YLabel = new String[YLength / YScale];
+
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg) {
+            if(msg.what == 0x1234){
+                drawComplex.this.invalidate();
+            }
+        };
+    };
+
+    public drawComplex(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        for(int i=0; i<YLabel.length; i++){
+            YLabel[i] = (i + 1) + "M/s";
+        }
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while(true){
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if(MainActivity.datas.size() >= MaxDataSize){
+                        MainActivity.datas.remove(0);
+                    }
+                    MainActivity.datas.add(1);
+                    handler.sendEmptyMessage(0x1234);
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+        paint.setColor(Color.BLUE);
+
+        //draw y aix
+        canvas.drawLine(XPoint, YPoint - YLength, XPoint, YPoint, paint);
+
+        //draw y arrow
+        canvas.drawLine(XPoint, YPoint - YLength, XPoint - 3, YPoint-YLength + 6, paint);  //arrow
+        canvas.drawLine(XPoint, YPoint - YLength, XPoint + 3, YPoint-YLength + 6 ,paint);
+
+        //draw mark and label
+        for(int i=0; i * YScale < YLength; i++) {
+            canvas.drawLine(XPoint, YPoint - i * YScale, XPoint + 5, YPoint - i * YScale, paint);  //mark
+
+            canvas.drawText(YLabel[i], XPoint - 50, YPoint - i * YScale, paint);//label
+        }
+
+        //draw x aix
+        canvas.drawLine(XPoint, YPoint, XPoint + XLength, YPoint, paint);
+        //System.out.println("Data.size = " + data.size()+" Datas.size = "+MainActivity.datas.size());
+        if(MainActivity.datas.size() > 1){
+            for(int i=1; i<MainActivity.datas.size(); i++){
+                canvas.drawLine(XPoint + (i-1) * XScale, YPoint - MainActivity.datas.get(i-1) * YScale,
+                        XPoint + i * XScale, YPoint - MainActivity.datas.get(i) * YScale, paint);
+            }
+        }
+    }
+}
