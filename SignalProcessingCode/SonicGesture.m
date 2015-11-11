e597b7e8b0df2fbe3159d74a1444760d07305ef5
Xuyi@MacAir <xruan@wisc.edu>
2015-11-10 23:42:52 -0600

    update processing file with target frequency

--- SignalProcessingCode/SonicGesture.m
+++ SignalProcessingCode/SonicGesture.m
@@ -2,12 +2,13 @@
 %% reference: 
 % http://www.mathworks.com/matlabcentral/fileexchange/38837-sound-analysis-with-matlab-implementation/content/Sound_Analysis.m
 % https://books.google.com/books?id=KXwAAQAAQBAJ&pg=PA11&lpg=PA11&dq=pcm+extension+matlab&source=bl&ots=o-AYfPikpI&sig=uXRjwJfsXW76KcHoJZPVeTWZaR8&hl=en&sa=X&ved=0CE4Q6AEwB2oVChMI58nT4O2EyQIVCPM-Ch2jcwlC#v=onepage&q=pcm%20extension%20matlab&f=false
+% plot in dB: https://dadorran.wordpress.com/2014/02/20/plotting-frequency-spectrum-using-matlab/
 
 clear
 
 % read in pcm file
 
-fid = fopen('test.pcm', 'r');
+fid = fopen('high_freq.pcm', 'r');
 sound = fread(fid, inf, 'int16', 0, 'ieee-be');
 fclose (fid);
 
@@ -35,26 +36,26 @@ title('The signal in the time domain')
 % play audio
 soundsc(sound, fs);
 
+
 % compute the DFT of signal 'x'
-rapX = fft(sound);
-fhat = (-N/2:N/2-1)/N;
+rapX = abs(fft(sound));
+%fhat = (-N/2:N/2-1)/N;
+fhat = [0: N-1]/N;
 fHz = fhat*fs;
+fhat_2 = ceil(N/2);
 
-% rapXB is the signal with bass being boosted
-rapXB = rapX;
-for i = 1:500/fs*N
-    rapXB(i) = abs(rapXB(i));
-end
-for j = N - round((500/fs*N)):N
-     rapXB(j) = abs(rapXB(j));
-end
+% target_freq
+freq_target = 18000;
+fhat_target = ceil((freq_target/fs)*N);
+low_bound = fhat_target - ceil(800/(fs/N));
+high_bound = fhat_target + ceil(800/(fs/N));
 
-fHz2 = fhat*fs;
 
 figure
-semilogy(fHz2, fftshift(abs(rapXB)))
-xlabel('Frequency in Hz');
-ylabel('Magnitude of the DFT coefficients');
+plot(fHz(low_bound:high_bound), 10*log10(rapX(low_bound:high_bound)))
+%plot(fHz(1:fhat_2), 10*log10(rapX(1:fhat_2)))
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (dB)');
 set(gca, 'FontName', 'Times New Roman', 'FontSize', 14)
 grid on
 title('DFT Frequency Analysis');
